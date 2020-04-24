b57d4391fe70cbd73d39cc7ad691c1a1dec6589e
Andy <andyleo97@gmail.com>
2020-04-24 11:34:47 -0500

    Working on logging

--- src/main/java/com/andyleo/multisolitaire/MultisolitaireApplication.java
+++ src/main/java/com/andyleo/multisolitaire/MultisolitaireApplication.java
@@ -1,10 +1,13 @@
 package com.andyleo.multisolitaire;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MultisolitaireApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(MultisolitaireApplication.class, args);
--- src/main/java/com/andyleo/multisolitaire/Services/UserService.java
+++ src/main/java/com/andyleo/multisolitaire/Services/UserService.java
@@ -2,11 +2,15 @@ package com.andyleo.multisolitaire.Services;
 
 import com.andyleo.multisolitaire.Domain.User;
 import com.andyleo.multisolitaire.Repositories.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+
 
     private final UserRepository userRepository;
 
